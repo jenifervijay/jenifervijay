c28d3ae2512af4ca51f4989f8bb3a1e7bf3a5dbe
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-01-22 15:39:27 +0530

    Create in1.c

---
+++ in1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,i,x[a],c;
+clrscr();
+scanf("%d %d",&a,&b);
+for(i=1;i<=a;i++)
+{
+x[i]=i;
+}
+for(i=1;i<=b;i++)
+{
+c=c+i;
+}
+printf("%d",c);
+getch();
+}  
