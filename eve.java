f659ecce941aecafe0cfa774a8c159e052923200
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-01-22 15:54:23 +0530

    Create eve.java

---
+++ eve.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class eve
+{
+public static void main(String args[]);
+{
+int i,n;
+Scanner s=new Scanner(System.in);
+Systen.out.printf("enter the  number:");
+n=s.nextInt();
+for(i=1;i<=n;i++)
+{
+if(i%2==0)
+{
+System.out.printl(i);
+}
+}
+}
+}
